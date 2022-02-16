e1d0c9132db4c1f676b16cc9d73567604de0a42c
Dror Kabely <sdrork123@gmail.com>
2022-02-17 00:23:41 +0200

    Added note card
    Signed-off-by: Dror Kabely <sdrork123@gmail.com>

---
+++ src/components/NoteCard.js
@@ -0,0 +1,30 @@
+import {  DeleteOutlined } from "@mui/icons-material";
+import { Card, CardContent, CardHeader, IconButton, Typography } from "@mui/material";
+import React from "react";
+
+const NoteCard = ({note, handleDelete}) => {
+    return (
+        <div>
+            <Card elevation={2}>
+                <CardHeader
+                    action={
+                        <IconButton onClick={() => handleDelete(note.id)}>
+                            <DeleteOutlined/>
+                        </IconButton>
+                    }
+                    title={note.title}
+                    subheader={note.category}
+                    />
+
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary">
+                        {note.details}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
+
+
+export default NoteCard;
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,6 @@
-import { Container, Grid, Paper } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import NoteCard from "../components/NoteCard";
 
 
 
@@ -12,15 +13,19 @@ function Notes() {
         .then(data => setNotes(data));
     }, []);
 
+    const handleDelete = async (id) => {
+        await fetch('http://localhost:8000/notes/' + id, { method: 'DELETE' });
+
+        const newNotes = notes.filter((note) => note.id != id);
+        setNotes(newNotes);
+    }
     return (
         <Container>
-            <Grid container>
+            <Grid container spacing={3}>
                 {notes.map((note) => {
                         return (
                             <Grid key={note.id} item xs={12} sm={6} lg={4}>
-                                <Paper>
-                                    <p >{note.title}</p>
-                                </Paper>
+                                <NoteCard note={note} handleDelete={handleDelete}/>
                             </Grid>
                         );
                     })
