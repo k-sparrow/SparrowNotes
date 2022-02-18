0b3b240a472f9ad347c61f36dc7808deee58a96e
Dror Kabely <sdrork123@gmail.com>
2022-02-18 22:37:29 +0200

    Added breakpoints to masonry
    Signed-off-by: Dror Kabely <sdrork123@gmail.com>

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -24,7 +24,11 @@ function Notes() {
     return (
         <Container>
             <Masonry
-                breakpointCols={3}
+                breakpointCols={{
+                    default: 3,
+                    1100: 2,
+                    700: 1
+                }}
                 className="my-masonry-grid"
                 columnClassName="my-masonry-grid_column"
                 >
